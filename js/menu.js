0943fdcd93d52b1d6de9de9fac2687c522b2edd8
maxjvjohansson <max.jv.johansson@gmail.com>
2024-11-18 20:00:25 +0100

    Added flag next to language selection in the menu and made it toggle between actual selected language

--- js/menu.js
+++ js/menu.js
@@ -6,3 +6,18 @@ scrollArrow.addEventListener('click', () => {
     const nextSection = document.querySelector('.underhero');
     nextSection.scrollIntoView({ behavior: 'smooth' });
 });
+
+// Toggles flag/selected language
+
+document.querySelector('.lang-toggle .flag-container').addEventListener('click', function () {
+    const ukFlag = document.getElementById('uk-flag');
+    const seFlag = document.getElementById('se-flag');
+
+    if (ukFlag.classList.contains('visible')) {
+        ukFlag.classList.remove('visible');
+        seFlag.classList.add('visible');
+    } else {
+        seFlag.classList.remove('visible');
+        ukFlag.classList.add('visible');
+    }
+});
