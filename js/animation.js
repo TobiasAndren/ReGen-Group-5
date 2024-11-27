391dc284a400a64cf6805cd1cff6daf6cb48c932
maxjvjohansson <max.jv.johansson@gmail.com>
2024-11-28 00:00:22 +0100

    Added back h1 parallax in hero for mobile users

--- js/animation.js
+++ js/animation.js
@@ -15,19 +15,19 @@ document.addEventListener("scroll", function() {
     let middle = document.querySelector('.middle');
     middle.style.transform = `scale(${1 + scrollPosition * 0.0002})`; 
 
-    // ReGen title text slide
-    document.addEventListener("scroll", function() {
+}
+        // ReGen title text slide
+        document.addEventListener("scroll", function() {
         let scrollPosition = window.scrollY;
         let h1 = document.querySelector('.hero h1'); 
         let movement = scrollPosition * 0.8; 
-    
+
         if (scrollPosition > 0) {
             h1.style.transform = `translateY(calc(-100% + ${movement}px))`; 
         } else {
             h1.style.transform = `translateY(-100%)`; 
         }
     });
-}
 })
 
 // Slideshow for motivation page
